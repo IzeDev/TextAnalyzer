3cb761ddb32ca0f195039cd8d2c61704b94b0b65
IzeDev <jimmy.gustafsson@live.com>
2021-01-04 14:23:57 +0100

    Fixed a few things

--- TextAnalyzer/Program.fs
+++ TextAnalyzer/Program.fs
@@ -1,14 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open TextAnalyzer
 
 [<EntryPoint>]
 let main argv =
-    let text = FileManager.Read.GetFileContent @"C:\Users\jimmy\source\repos\TextAnalyzer\TextAnalyzer\AppData\text.txt"
+    let text = FileManager.Read.GetFileContent @"C:\Users\jimmy\Source\TextAnalyzer\TextAnalyzer\AppData\text.txt"
     printfn "%s" text
-    let apa = TextAnalyzer.Main.doSomething 5
+    let apa = TextAnalyzer.analyzeText text
 
-
-
-    Console.ReadKey() |> ignore
+    Console.Read() |> ignore
     0 // return an integer exit code
--- TextAnalyzer/TextAnalyzer.fs
+++ TextAnalyzer/TextAnalyzer.fs
@@ -1,12 +1,30 @@
 namespace TextAnalyzer
     module Data =
-        type WordScore =
+        type PhraseScore =
             {
-                Word : string
-                score : int32
+                Phrase : string
+                Score : int32
             }
 
+    module TextAnalyzer =
+        open Data
+        open System
 
-    module Main =
-        let doSomething x = x * x
-        
+        module private TextAnalyzer =
+            let analyzeByWord (text : string) =
+                    text.Split() |>
+                    Array.map(fun x -> x.ToLowerInvariant().ToCharArray()) |>
+                    Array.map(fun x -> x |>Array.filter(fun y -> not(Char.IsPunctuation(y) || Char.IsSeparator(y)))) |>
+                    Array.map(fun x -> new string(x)) |>
+                    Array.filter(fun x -> not(String.IsNullOrWhiteSpace(x))) |>
+                    Array.countBy(id) |>
+                    Array.map(fun (x,y) -> { Phrase = x; Score = y })              
+
+            let analyzeBySentence x = x
+
+            let analyzeByWordCombination x = x
+
+        let analyzeText (text : string) =
+            let byWordAnalysis = TextAnalyzer.analyzeByWord text
+
+            text
